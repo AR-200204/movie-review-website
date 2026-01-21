ba77236b77ac317e81ea01eb914a04b045b89fbe
AR-200204 <aritharandiw2021@gmail.com>
2026-01-21 10:52:54 +0530

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  // site: 'https://yourusername.github.io',
-  // base: '/movie-site',
+  site: 'https://ar-200204.github.io',
+  base: '/movie-review-website',
   output: 'static',
   build: {
     assets: 'assets'
